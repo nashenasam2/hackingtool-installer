80a024e45ba41f9a37fb212de4f0df54d4a5165c
Ender <66910478+BotNetX666@users.noreply.github.com>
2020-06-25 16:23:41 +0300

    Update webhacking.sh

--- webhacking/webhacking.sh
+++ webhacking/webhacking.sh
@@ -5,15 +5,15 @@ if [[ $option == 1 ]];
 then
 clear
 echo "Installing Sqlmap...";
-pkg install unstable-repo
-pkg install sqlmap
+apt install unstable-repo
+apt install sqlmap
 echo "Sqlmap Installed! Type sqlmap To Run It"
 elif [[ $option == 2 ]]; 
 then
 cd $HOME
 echo "Installing Hunner..."
-pkg install git
-pkg install python
+apt install git
+apt install python
 git clone https://github.com/b3-v3r/Hunner
 cd Hunner
 python hunner.py
